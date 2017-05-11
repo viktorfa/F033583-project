42eb353e449db8125f9aa214259333e794b481e4
viktorfa <vikfand@gmail.com>
2017-05-11 14:06:22 +0800

    Make search log a module instead of class as it should act as a "Singleton"

--- retrieval/search_log.py
+++ retrieval/search_log.py
@@ -1,50 +1,55 @@
-class SearchLog:
-    def __init__(self):
-        self.query_log = {}
-        self.id_count = 0
-        self.query_index = {}
+id_count = 0
+query_log = {}
+query_index = {}
 
-    def register_click(self, query_id, song_id):
-        self.query_log[query_id].register_click(song_id)
 
-    def register_play(self, query_id, song_id):
-        self.query_log[query_id].register_play(song_id)
+def register_click(query_id, song_id):
+    query_log[query_id].register_click(song_id)
 
-    def register_query(self, query):
-        query_id = self._get_query_id()
-        search_log_query = SearchLogQuery(query, query_id)
-        self.query_log[query_id] = search_log_query
 
-        if query.get_query() in self.query_index.keys():
-            self.query_index[query.get_query()].append(search_log_query)
-        else:
-            self.query_index[query.get_query()] = [search_log_query]
-        return query_id
+def register_play(query_id, song_id):
+    query_log[query_id].register_play(song_id)
 
-    def get_songs_clicked_for_query(self, query):
-        result = []
-        if query not in self.query_index.keys():
-            return result
-        else:
-            for search_log_query in self.query_index[query]:
-                result.extend(search_log_query.get_songs_clicked())
 
+def register_query(query):
+    query_id = _get_query_id()
+    search_log_query = SearchLogQuery(query, query_id)
+    query_log[query_id] = search_log_query
+
+    if query.get_query() in query_index.keys():
+        query_index[query.get_query()].append(search_log_query)
+    else:
+        query_index[query.get_query()] = [search_log_query]
+    return query_id
+
+
+def get_songs_clicked_for_query(query):
+    result = []
+    if query not in query_index.keys():
         return result
+    else:
+        for search_log_query in query_index[query]:
+            result.extend(search_log_query.get_songs_clicked())
 
-    def get_songs_played_for_query(self, query):
-        result = []
-        if query not in self.query_index.keys():
-            return result
-        else:
-            for search_log_query in self.query_index[query]:
-                result.extend(search_log_query.get_songs_played())
+    return result
 
+
+def get_songs_played_for_query(query):
+    result = []
+    if query not in query_index.keys():
         return result
+    else:
+        for search_log_query in query_index[query]:
+            result.extend(search_log_query.get_songs_played())
 
-    def _get_query_id(self):
-        temp = self.id_count
-        self.id_count += 1
-        return temp
+    return result
+
+
+def _get_query_id():
+    global id_count
+    temp = id_count
+    id_count += 1
+    return temp
 
 
 class SearchLogQuery:
--- tests/retrieval/test_search_log.py
+++ tests/retrieval/test_search_log.py
@@ -1,24 +1,24 @@
 import unittest
 
-from retrieval.search_log import SearchLog
+from retrieval import search_log
 
 
 class TestSearchLogBasic(unittest.TestCase):
     def setUp(self):
-        self.search_log = SearchLog()
+        pass
 
     def test_register_query(self):
-        id_count = self.search_log.id_count
+        id_count = search_log.id_count
         query = 'bad girl'
         query_2 = 'heya'
         query_object = QueryMock(query)
         query_object_2 = QueryMock(query_2)
-        query_id = self.search_log.register_query(query_object)
-        query_id_2 = self.search_log.register_query(query_object_2)
-        self.assertEqual(id_count + 2, self.search_log.id_count)
+        query_id = search_log.register_query(query_object)
+        query_id_2 = search_log.register_query(query_object_2)
+        self.assertEqual(id_count + 2, search_log.id_count)
         self.assertNotEqual(query_id, query_id_2)
-        self.assertListEqual(self.search_log.get_songs_clicked_for_query(query_object), [])
-        self.assertListEqual(self.search_log.get_songs_played_for_query(query_object), [])
+        self.assertListEqual(search_log.get_songs_clicked_for_query(query_object), [])
+        self.assertListEqual(search_log.get_songs_played_for_query(query_object), [])
 
     def test_register_click(self):
         song_id = 0
@@ -26,15 +26,15 @@ class TestSearchLogBasic(unittest.TestCase):
         query_object = QueryMock(query)
         query_object_2 = QueryMock(query)
 
-        query_id = self.search_log.register_query(query_object)
-        query_id_2 = self.search_log.register_query(query_object_2)
+        query_id = search_log.register_query(query_object)
+        query_id_2 = search_log.register_query(query_object_2)
 
-        self.search_log.register_click(query_id, song_id)
-        self.search_log.register_click(query_id, song_id + 1)
-        self.search_log.register_click(query_id_2, song_id + 2)
+        search_log.register_click(query_id, song_id)
+        search_log.register_click(query_id, song_id + 1)
+        search_log.register_click(query_id_2, song_id + 2)
 
         expected = [song_id, song_id + 1, song_id + 2]
-        actual = self.search_log.get_songs_clicked_for_query(query)
+        actual = search_log.get_songs_clicked_for_query(query)
 
         self.assertListEqual(actual, expected)
 
