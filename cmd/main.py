033ffe7f7ea2989243cf76a588e22018ab3e6a31
viktorfa <vikfand@gmail.com>
2017-06-20 18:53:01 +0800

    Make command line interface for creating indixes.

---
+++ cmd/main.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+module_path = os.path.abspath(os.path.join('.'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+import argparse
+from indexer.inverted_index import IndexProvider
+
+index_fields = [
+    ['title'],
+    ['lyrics'],
+    ['artist'],
+    ['artist', 'lyrics'],
+    ['artist', 'title'],
+    ['title', 'lyrics'],
+    ['title', 'lyrics', 'artist']
+]
+
+parser = argparse.ArgumentParser('Start indexing scraper output.')
+
+parser.add_argument('pos_arg', type=str,
+                    help='File name of the output file from the scraper.')
+
+args = parser.parse_args()
+
+print(args.pos_arg)
+
+ip = IndexProvider(args.pos_arg)
+
+for index in index_fields:
+    ip.create_inverted_index(index)
